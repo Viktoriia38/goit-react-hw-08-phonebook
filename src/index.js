b9d06b1c35c4f7c8c846efbffdcfd9b3dc46b438
Viktoriia38 <bva922@ukr.net>
2023-03-02 18:49:54 +0200

    + changed index

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter
-      // basename="/goit-react-hw-05-movies/"
-      >
+      <BrowserRouter basename="/goit-react-hw-05-movies/">
         <App />
       </BrowserRouter>
     </PersistGate>
