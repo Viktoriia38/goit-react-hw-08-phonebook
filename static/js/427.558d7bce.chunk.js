a9dfe474047d1656f3e0bccf287daca2897b53da
Viktoriia38 <116452275+Viktoriia38@users.noreply.github.com>
2023-03-02 16:51:59 +0000

    Deploying to gh-pages from @ Viktoriia38/goit-react-hw-08-phonebook@b9d06b1c35c4f7c8c846efbffdcfd9b3dc46b438 🚀

---
+++ static/js/427.558d7bce.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[427],{7427:function(e,o,a){a.r(o),a.d(o,{default:function(){return c}});var n="HomePage_homePageTitle__RbMmj",t="HomePage_homePageImage__gOAc-",_=a(184);var c=function(){return(0,_.jsxs)(_.Fragment,{children:[(0,_.jsx)("h1",{className:n,children:"Phonebook"}),(0,_.jsx)("div",{className:t})]})}}}]);
+//# sourceMappingURL=427.558d7bce.chunk.js.map
